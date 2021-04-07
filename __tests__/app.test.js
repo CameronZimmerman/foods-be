955db64a48c2b829e16b4497149d900e363c4dc7
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-04-07 09:07:19 -0700

    working test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,14 @@ const request = require("supertest");
 const setup = require("../lib/utils/setup.js");
 const app = require("../lib/app.js");
 
-Describe("crud routes", () => {
-  beforeAll(() => {
+describe("crud routes", () => {
+  beforeEach(() => {
     return setup();
   });
 
   test("should create a food and respond with it's contents", async () => {
     const res = await request(app)
-      .post("http://localhost:9999/api/foods")
+      .post("/api/foods")
       .send({ name: "bagel", tastiness: 100 });
     expect(res.body).toEqual({
       id: "1",
